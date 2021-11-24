d1b7a97f7ab5788296374f6fb7b73ff45ac08c42
Olga Lukashenko <lukashenko.93@bk.ru>
2021-11-24 22:05:00 +0300

    Added bug file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int sumTransfer = 500_000_000;
+        int total = balance + sumTransfer;
+        System.out.println(total);
+    }
+}
